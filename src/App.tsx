7c53fc94edcdfb39b7993607dc924bb4534ba2d1
JaeranKIM <devjaeran@gmail.com>
2023-09-05 17:03:11 +0900

    Refactoring: component 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import TodoList from "./components/TodoList";
+import ToDoList from "./components/ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -70,7 +70,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <TodoList />
+      <ToDoList />
     </>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,57 +1,22 @@
-import { useForm } from "react-hook-form";
-import {
-  atom,
-  useRecoilState,
-  useRecoilValue,
-  useSetRecoilState,
-} from "recoil";
+import { useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
-const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-
-interface IForm {
-  toDo: string;
-}
-
-interface IToDo {
-  text: string;
-  id: number;
-  category: "TO_DO" | "DOING" | "DONE";
-}
-
-function TodoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState); //toDos: Array, setToDos: setter|Updater Fn
-  // const value = useRecoilValue(toDoState); //state 받아오기
-  // const modFn = useSetRecoilState(toDoState); //state 변경하기
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-  const handleValid = ({ toDo }: IForm) => {
-    setToDos(oldToDos => [
-      { text: toDo, id: Date.now(), category: "TO_DO" },
-      ...oldToDos,
-    ]);
-    setValue("toDo", "");
-  };
+function ToDoList() {
+  const toDos = useRecoilValue(toDoState);
   return (
     <div>
-      <h1>ToDo</h1>
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input
-          {...register("toDo", {
-            required: "Please write a To Do",
-          })}
-          placeholder="Write a to do"
-        />
-        <button>Add</button>
-      </form>
+      <h1>To Dos</h1>
+      <hr />
+      <CreateToDo />
       <ul>
         {toDos.map(toDo => (
-          <li key={toDo.id}>{toDo.text}</li>
+          <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
     </div>
   );
 }
 
-export default TodoList;
+export default ToDoList;
